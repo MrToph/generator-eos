5595793c8755e2ebf1d7e8d96e5170e1e161a7b7
Christoph Michel <christoph.michel.1992@gmail.com>
2018-11-05 23:37:28 +0100

    Fix table dump script

--- generators/app/templates/scripts/table.js
+++ generators/app/templates/scripts/table.js
@@ -1,27 +1,36 @@
+const fs = require(`fs`)
+const path = require(`path`)
 const { api } = require(`../config`)
-const { getErrorDetail } = require(`../utils`)
+const { getErrorDetail, sendTransaction } = require(`../utils`)
 
 const { CONTRACT_ACCOUNT } = process.env
-const ROWS_LIMIT = 99999
+const contractDir = `./contract`
 
-// TODO: Fix this and read tables from ABI, currently broken
 async function script() {
-    try {
-        const result = await api.rpc.get_table_rows({
-            json: true,
-            code: CONTRACT_ACCOUNT,
-            scope: CONTRACT_ACCOUNT,
-            table: `table`,
-            table_key: `table_key` /* is ignored? */,
-            lower_bound: 0,
-            upper_bound: -1,
-            limit: ROWS_LIMIT,
-        })
-        const { rows } = result
-        rows.forEach(row => console.log(JSON.stringify(row, null, 4)))
-    } catch (error) {
-        console.error(`${getErrorDetail(error)}`)
+    const tableName = process.argv[2]
+    const abi = JSON.parse(fs.readFileSync(path.join(contractDir, `testi.abi`)), `utf8`)
+    const validTableNames = abi.tables
+        ? abi.tables.map(({ name }) => `"${name}"`).join(` `)
+        : `No table names`
+    if (!tableName) {
+        console.log(
+            `Please pass a valid table name as an argument to this script.\nExample: npm run table -- games\nValid table names: ${validTableNames}`,
+        )
+        return
     }
+
+    // https://developers.eos.io/eosio-nodeos/reference#get_table_rows
+    const result = await api.rpc.get_table_rows({
+        json: true,
+        code: CONTRACT_ACCOUNT,
+        scope: process.argv[3] || CONTRACT_ACCOUNT,
+        table: tableName,
+        lower_bound: 0,
+        upper_bound: -1,
+        limit: 9999,
+        index_position: 1,
+    })
+    console.dir(result.rows)
 }
 
 script()
